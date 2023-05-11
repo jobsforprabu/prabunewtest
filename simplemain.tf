a6390897fc7ecb5374b2d1b515e0ae987ada93c4
prabuganesh <jobsforprabu@gmail.com>
2023-05-11 07:49:57 +0530

    test

--- simplemain.tf
+++ simplemain.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"
     access_key = "AKIA4M2WLVU2ESL2Y5PL"
     secret_key = "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" 
-
+}
 
 resource "aws_vpc" "myprabu" {
     cidr_block = "10.0.0.0/16"
