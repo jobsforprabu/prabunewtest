057b736125a2fbd335667ed9918ff4a97b27e457
prabuganesh <jobsforprabu@gmail.com>
2023-05-11 07:40:06 +0530

    terraform backend

--- simplemain.tf
+++ simplemain.tf
@@ -4,6 +4,15 @@ provider "aws" {
     secret_key = "arvhWwF8A00Nr0z+J4fONvenU5DsBJ7WSlpTliZo" 
 }
 
+terraform {
+  backend "s3" {
+    bucket = "prabu1111111212121"
+    key = "terraform.tfstate"
+    region = "us-east-1"
+    
+  }
+}
+
 resource "aws_vpc" "myprabu" {
     cidr_block = "10.0.0.0/16"
     instance_tenancy = "default"
